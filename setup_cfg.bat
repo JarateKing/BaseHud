0db860d8536064fa9a8a7555ad7ad065e135ee28
JarateKing <scmacintosh@upei.ca>
2018-10-30 11:00:22 -0300

    added mastercomfig compatibility

--- setup_cfg.bat
+++ setup_cfg.bat
@@ -1,3 +1,4 @@
+:: always add / create class cfg's in cfg
 type dev\ref\add_to_class.cfg >> ..\..\cfg\scout.cfg
 type dev\ref\add_to_class.cfg >> ..\..\cfg\soldier.cfg
 type dev\ref\add_to_class.cfg >> ..\..\cfg\pyro.cfg
@@ -7,3 +8,14 @@ type dev\ref\add_to_class.cfg >> ..\..\cfg\engineer.cfg
 type dev\ref\add_to_class.cfg >> ..\..\cfg\medic.cfg
 type dev\ref\add_to_class.cfg >> ..\..\cfg\sniper.cfg
 type dev\ref\add_to_class.cfg >> ..\..\cfg\spy.cfg
+
+:: mastercomfig compatibility, since they are overridden
+if exist ..\..\cfg\scout_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\scout_c.cfg )
+if exist ..\..\cfg\soldier_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\soldier_c.cfg )
+if exist ..\..\cfg\pyro_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\pyro_c.cfg )
+if exist ..\..\cfg\demoman_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\demoman_c.cfg )
+if exist ..\..\cfg\heavyweapons_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\heavyweapons_c.cfg )
+if exist ..\..\cfg\engineer_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\engineer_c.cfg )
+if exist ..\..\cfg\medic_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\medic_c.cfg )
+if exist ..\..\cfg\sniper_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\sniper_c.cfg )
+if exist ..\..\cfg\spy_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\spy_c.cfg )
