541291e2c1f4f5d622b1eb0cea5241f23996a845
JarateKing <scmacintosh@upei.ca>
2018-11-01 11:32:53 -0300

    added mastercomfig class cfg's for setup

--- setup_cfg.bat
+++ setup_cfg.bat
@@ -20,7 +20,9 @@ if exist ..\..\cfg\medic_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\medi
 if exist ..\..\cfg\sniper_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\sniper_c.cfg )
 if exist ..\..\cfg\spy_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\spy_c.cfg )
 
+:: check all custom folders as well
 for /d %%i in (..\*) do (
+	:: standard class cfg's
 	if exist %%i\cfg\scout.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\scout.cfg )
 	if exist %%i\cfg\soldier.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\soldier.cfg )
 	if exist %%i\cfg\pyro.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\pyro.cfg )
@@ -30,4 +32,15 @@ for /d %%i in (..\*) do (
 	if exist %%i\cfg\medic.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\medic.cfg )
 	if exist %%i\cfg\sniper.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\sniper.cfg )
 	if exist %%i\cfg\spy.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\spy.cfg )
+	
+	:: mastercomfig compatibility
+	if exist %%i\cfg\scout_c.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\scout_c.cfg )
+	if exist %%i\cfg\soldier_c.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\soldier_c.cfg )
+	if exist %%i\cfg\pyro_c.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\pyro_c.cfg )
+	if exist %%i\cfg\demoman_c.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\demoman_c.cfg )
+	if exist %%i\cfg\heavyweapons_c.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\heavyweapons_c.cfg )
+	if exist %%i\cfg\engineer_c.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\engineer_c.cfg )
+	if exist %%i\cfg\medic_c.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\medic_c.cfg )
+	if exist %%i\cfg\sniper_c.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\sniper_c.cfg )
+	if exist %%i\cfg\spy_c.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\spy_c.cfg )
 )
