d8bcbecf54250edd931b1ea8db668668753ca1e9
JarateKing <scmacintosh@upei.ca>
2018-11-01 11:31:07 -0300

    check inside custom folders for class cfgs

--- setup_cfg.bat
+++ setup_cfg.bat
@@ -19,3 +19,15 @@ if exist ..\..\cfg\engineer_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\e
 if exist ..\..\cfg\medic_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\medic_c.cfg )
 if exist ..\..\cfg\sniper_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\sniper_c.cfg )
 if exist ..\..\cfg\spy_c.cfg ( type dev\ref\add_to_class.cfg >> ..\..\cfg\spy_c.cfg )
+
+for /d %%i in (..\*) do (
+	if exist %%i\cfg\scout.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\scout.cfg )
+	if exist %%i\cfg\soldier.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\soldier.cfg )
+	if exist %%i\cfg\pyro.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\pyro.cfg )
+	if exist %%i\cfg\demoman.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\demoman.cfg )
+	if exist %%i\cfg\heavyweapons.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\heavyweapons.cfg )
+	if exist %%i\cfg\engineer.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\engineer.cfg )
+	if exist %%i\cfg\medic.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\medic.cfg )
+	if exist %%i\cfg\sniper.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\sniper.cfg )
+	if exist %%i\cfg\spy.cfg ( type dev\ref\add_to_class.cfg >> %%i\cfg\spy.cfg )
+)
