14188d4ccf402bba4d514e21ee2b795afeb9d5e0
JarateKing <scmacintosh@upei.ca>
2018-11-11 18:53:01 -0400

    added basic shell setup cfg

---
+++ setup_cfg.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# always add / create class cfg's in cfg
+cat dev/ref/add_to_class.cfg >> ../../cfg/scout.cfg
+cat dev/ref/add_to_class.cfg >> ../../cfg/soldier.cfg
+cat dev/ref/add_to_class.cfg >> ../../cfg/pyro.cfg
+cat dev/ref/add_to_class.cfg >> ../../cfg/demoman.cfg
+cat dev/ref/add_to_class.cfg >> ../../cfg/heavyweapons.cfg
+cat dev/ref/add_to_class.cfg >> ../../cfg/engineer.cfg
+cat dev/ref/add_to_class.cfg >> ../../cfg/medic.cfg
+cat dev/ref/add_to_class.cfg >> ../../cfg/sniper.cfg
+cat dev/ref/add_to_class.cfg >> ../../cfg/spy.cfg
